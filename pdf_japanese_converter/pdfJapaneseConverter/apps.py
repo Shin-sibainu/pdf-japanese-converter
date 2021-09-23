7d9edd7219d54e7e7dfe408d9d94c0997d8209fa
Shin-sibainu <shincode0712@gmail.com>
2021-09-23 19:04:10 +0900

    django installed

---
+++ pdf_japanese_converter/pdfJapaneseConverter/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PdfjapaneseconverterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pdfJapaneseConverter'
---
+++ pdf_japanese_converter/pdfJapaneseConverter/pdf-ja-converter.py
@@ -0,0 +1,29 @@
+from pdfminer.high_level import extract_text
+from google.cloud import translate
+
+pdf_text = extract_text("./pdf/test.pdf")
+
+
+def translate_text(target_text):
+    """Translating Text."""
+
+    from google.cloud import translate
+
+    client = translate.TranslationServiceClient()
+    parent = client.location_path("beaming-edition-325211", "global")
+    text = target_text
+
+    response = client.translate_text(
+        parent=parent,
+        contents=[text],
+        mime_type="text/plain",
+        source_language_code="en-US",
+        target_language_code="ja",
+    )
+
+    # Display the translation for each input text provided
+    for translation in response.translations:
+        print("Translated text: {}".format(translation.translated_text))
+
+
+translate_text(pdf_text)
