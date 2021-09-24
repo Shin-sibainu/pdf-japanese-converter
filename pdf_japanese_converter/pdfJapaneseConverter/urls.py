264b6d915316a3a08a8d87f2e451dfba140d3d67
Shin-sibainu <shincode0712@gmail.com>
2021-09-24 09:59:27 +0900

    form added. but cant understand pdf-data text diplay

--- pdf_japanese_converter/pdfJapaneseConverter/urls.py
+++ pdf_japanese_converter/pdfJapaneseConverter/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, PDFJapaneseConvert
+
+app_name = 'pdfJapaneseConverter'
 
 urlpatterns = [
     path("", HomeView),
+    path("convert/", PDFJapaneseConvert, name='convert'),
 ]
--- pdf_japanese_converter/pdfJapaneseConverter/views.py
+++ pdf_japanese_converter/pdfJapaneseConverter/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
+from pdfminer.high_level import extract_text
 
 # Create your views here.
 
 
 def HomeView(request):
     return render(request, "index.html", {})
+
+
+def translate_text(target_text):
+    """Translating Text."""
+
+    from google.cloud import translate
+
+    client = translate.TranslationServiceClient()
+    parent = client.location_path("beaming-edition-325211", "global")
+    text = target_text
+
+    response = client.translate_text(
+        parent=parent,
+        contents=[text],
+        mime_type="text/plain",
+        source_language_code="en-US",
+        target_language_code="ja",
+    )
+
+    # Display the translation for each input text provided
+    for translation in response.translations:
+        print("Translated text: {}".format(translation.translated_text))
+
+
+def PDFJapaneseConvert(request):
+    # 翻訳するアルゴリズム
+    # 必要なもの：pdfデータ（中身のテキストはライブラリで変換）。translate_textのmodule
+    fileInfo = request.FILES["file"]
+    data = {
+        "file": fileInfo,
+    }
+    return render(request, "example.html", data)
